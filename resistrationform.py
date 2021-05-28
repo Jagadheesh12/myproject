8ce24a55382aabbbd5030cebda1a9876e620fa23
Jagadheesh12 <82741930+Jagadheesh12@users.noreply.github.com>
2021-05-28 16:35:55 +0530

    first commit

---
+++ resistrationform.py
@@ -0,0 +1,62 @@
+<head>   
+<title>  
+Registration Page  
+</title>  
+</head>  
+<body bgcolor="skyblue">  
+<br>  
+<br>  
+<form>  
+  
+<label> Firstname </label>         
+<input type="text" name="firstname" size="15"/> <br> <br>  
+<label> Lastname: </label>         
+<input type="text" name="lastname" size="15"/> <br> <br>  
+  
+<label>   
+Course :  
+</label>   
+<select>  
+<option value="Course">Course</option>  
+<option value="B.E">B.E</option>  
+<option value="M.E">M.E</option>  
+<option value="B.Tech">B.Tech</option>  
+<option value="M.Tech">M.Tech</option>  
+<option value="MBA">MBA</option>  
+</select>  
+  
+<br>  
+<br>  
+<label>   
+Gender :  
+</label><br>  
+<input type="radio" name="male"/> Male <br>  
+<input type="radio" name="female"/> Female <br>  
+<input type="radio" name="other"/> Other  
+<br>  
+<br>  
+  
+<label>   
+Phone :  
+</label>  
+<input type="text" name="country code"  value="+91" size="2"/>   
+<input type="text" name="phone" size="15"/> <br> <br>  
+Address  
+<br>  
+<textarea cols="50" rows="5" value="address">  
+</textarea>  
+<br> <br>  
+Email:  
+<input type="email" id="email" name="email"/> <br>    
+<br> <br>  
+Password:  
+<input type="Password" id="pass" name="pass"> <br>   
+<br> <br>  
+Confirm password:  
+<input type="Password" id="repass" name="repass"> <br> <br>  
+<input type="button" value="Submit"/> 
+</form>  
+</body>  
+</html<Html>  
+  
+
